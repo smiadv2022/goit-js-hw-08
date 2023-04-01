a2585dbea90f22ecf13e93c587a22b4457e30514
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-02 00:57:03 +0300

    tasck1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,51 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+
+console.log(galleryItems);
+const divRefs = document.querySelector('.gallery');
+function createGalleryList(items) {
+  return items
+    .map(
+      item => `<li class="gallery__item">
+     <a class="allery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}"
+     data-source="${item.original}"
+     alt="${item.description}" />
+     </a>
+     </li>`
+    )
+    .join('');
+}
+const addGalleryList = createGalleryList(galleryItems);
+divRefs.innerHTML = addGalleryList;
+divRefs.addEventListener('click', onImgFocus);
+function onImgFocus(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  function listener(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      console.log('+');
+    }
+  }
+  const instance = basicLightbox.create(
+    `
+  <img src ="${event.target.dataset.source}" width="800" height="600" >
+  `,
+    {
+      onShow: instance => {
+        divRefs.addEventListener('keydown', listener);
+      },
+      onClose: instance => {
+        divRefs.removeEventListener('keydown', listener);
+      },
+    }
+  );
+  instance.show();
+}
---
+++ src/js/02-video.js
@@ -0,0 +1,11 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
