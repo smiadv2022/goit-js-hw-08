5732f0a11c78e6d319d55fa20a0cd7dc50615166
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-03 12:54:58 +0300

    fix1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 const form =document.querySelector('.feedback-form');
-
+initForm();
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
@@ -14,13 +14,36 @@ function onFormInput(event){
   try {
     localStorage.setItem(STORAGE_KEY, formDataJson);
   } catch (e) {
-    console.error('LocalStorage not supported');
+    console.error('LocalStorage not supported 1');
   }
 
 }
 function onFormSubmit(){
   const saveFormSubmit = localStorage.getItem(STORAGE_KEY);
+  console.log("submit:", saveFormSubmit);
+  // console.log("form:", form.elements);
   if(saveFormSubmit){
-    console.log(saveFormSubmit);
+    localStorage.removeItem(STORAGE_KEY);
+    // console.log("sub-", saveFormSubmit);
+
   }
+}
+function initForm (){
+let userFilter = localStorage.getItem(STORAGE_KEY);
+// console.log("us:", userFilter);
+try {
+  if (userFilter){
+  userFilter=JSON.parse(userFilter);
+  Object.entries(userFilter).forEach(([name, value])=>{
+form.elements[name].value = value;
+// console.log("from-",form.elements[name].value);
+
+  });
+}
+}
+
+catch (e) {
+  console.error('LocalStorage not supported 2');
+}
+
 }
