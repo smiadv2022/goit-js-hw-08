bbcf5d2b658b73d06efc313ff72e9a27be53fc45
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-05 11:45:00 +0300

    fix2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,44 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
-const form =document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
+
 initForm();
+
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-function onFormInput(event){
-  const formData ={
-    email:form.elements.email.value,
-    message: form.elements.message.value,
-  };
-  const formDataJson = JSON.stringify(formData);
+function onFormInput(event) {
   try {
+    const formData = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    const formDataJson = JSON.stringify(formData);
+
     localStorage.setItem(STORAGE_KEY, formDataJson);
   } catch (e) {
     console.error('LocalStorage not supported 1');
   }
-
 }
-function onFormSubmit(){
+
+function onFormSubmit(event) {
+  event.preventDefault();
   const saveFormSubmit = localStorage.getItem(STORAGE_KEY);
-  console.log("submit:", saveFormSubmit);
-  // console.log("form:", form.elements);
-  if(saveFormSubmit){
+  console.log('submit:', saveFormSubmit);
+
+  if (saveFormSubmit) {
     localStorage.removeItem(STORAGE_KEY);
-    // console.log("sub-", saveFormSubmit);
-
   }
-}
-function initForm (){
-let userFilter = localStorage.getItem(STORAGE_KEY);
-// console.log("us:", userFilter);
-try {
-  if (userFilter){
-  userFilter=JSON.parse(userFilter);
-  Object.entries(userFilter).forEach(([name, value])=>{
-form.elements[name].value = value;
-// console.log("from-",form.elements[name].value);
-
-  });
-}
+  form.reset();
 }
 
-catch (e) {
-  console.error('LocalStorage not supported 2');
-}
-
+function initForm() {
+  try {
+    const userData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    Object.entries(userData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  } catch (e) {
+    console.error('LocalStorage not supported 2');
+  }
 }
